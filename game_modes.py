b1c0ce862e5622c1474fcf19fceada8dd0135c67
nsdk05 <kirilldenisov2288@gmail.com>
2025-11-30 22:10:22 +0500

    Создал game_modes.py и добавил базовый класс Game с логикой игры
    Реализовал:
    - Класс Game с полями игроков
    - Переключение ходов между игроками
    - Обработку выстрелов
    - Проверку победы

---
+++ game_modes.py
@@ -0,0 +1,31 @@
+import random
+from game_logic import GameBoard
+
+class Game:
+    def __init__(self):
+        self.board1=GameBoard()
+        self.board2=GameBoard()
+        self.current_player=1
+        self.game_over=False
+        self.winner=None
+
+    def switch_player(self):
+        if self.current_player==1:
+            self.current_player=2
+        else:
+            self.current_player=1
+
+    def make_shot(self,x,y):
+        if self.game_over:
+            return "game_over"
+        if self.current_player==1:
+            target_board=self.board2
+        else:
+            target_board=self.board1
+        result=target_board.shoot(x,y)
+        if result=="win":
+            self.game_over=True
+            self.winner=self.current_player
+        if result=="miss":
+            self.switch_player()
+        return result
